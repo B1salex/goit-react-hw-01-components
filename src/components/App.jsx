7d018dd1f2e877200397d6d95f5b5adb8af08fc0
Alex Mandebura <xenovidbis@gmail.com>
2023-01-11 09:27:19 +0200

    render Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Profile } from 'components/Profile/Profile';
+
+// eslint-disable-next-line
+import { FriendList } from 'components/FriendList/FriendList';
+// eslint-disable-next-line
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+// eslint-disable-next-line
+import { Statistics } from 'components/Statistics/Statistics';
+// eslint-disable-next-line
+import { TransactionHistore } from 'components/TransactionHistory/TransactionHstory';
+
+import user from '../data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
